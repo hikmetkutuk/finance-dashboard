fa062ebe225488e79aacf8bdcd95877ed1003e60
Hikmet Kütük <hikmetkutuk@gmail.com>
2024-04-13 23:42:04 +0300

    react router and navbar

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,10 +1,29 @@
+import { createTheme } from "@mui/system";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Navbar from "@/scenes/navbar";
 
 function App() {
-
+  const theme = useMemo(() => createTheme(themeSettings), []);
   return (
-    <>        
+    <>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<div>dashboard</div>} />
+              <Route path="/predictions" element={<div>predictions</div>} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
